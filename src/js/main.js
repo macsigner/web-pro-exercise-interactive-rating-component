ead81fa0e64948bf44e2eb88bc5f611605c0f14b
Mac Signer <macsigner@gmx.ch>
2022-05-04 08:39:24 +0200

    Add card flip

--- src/js/main.js
+++ src/js/main.js
@@ -3,3 +3,22 @@ import '/src/scss/main.scss'
 
 document.documentElement.classList.remove('no-js');
 document.documentElement.classList.add('js');
+
+const form = document.querySelector('.card form');
+
+form.addEventListener('submit', e => {
+    e.preventDefault();
+
+    let wrapper = e.target.closest('.flip-wrapper');
+    wrapper.classList.add('flip-wrapper--flip');
+});
+
+form.addEventListener('change', e => {
+    let formData = new FormData(e.target.closest('form'));
+
+    let nlDataCount = document.querySelectorAll('[data-count]');
+
+    nlDataCount.forEach(el => {
+        el.innerHTML = formData.get('rating');
+    });
+})
